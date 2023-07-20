2ceeeec72eef85b1ffc1d74c4db79619ad603373
infoparity <infoparity@protonmail.com>
2023-07-19 23:26:48 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Proveably Random Raffle Contracts
+
+## About
+This code is to create a provably random smart contract lottery..
+
+## Current Timestamp
+https://youtu.be/sas02qSFZ74?t=14938
+
+## What we want it to do?
+1. Users can enter by paying for a ticket.
+    - The ticket fees are going to go to the winner during the draw.
+2. After X period of time, the lottery will automatically draw a winner.
+    - And this will be done programatically.
+3. Using Chainlink VRF & Chainlink Automation to generate randomness and have time-based triggers.
---
+++ src/Raffle.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+/**
+ * @title A sample raffle contract
+ * @author infoparity
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle is VRFConsumerBaseV2 {
+    error Raffle__NotEnoughEthSent();
+    error Raffle__NotEnoughTimePassed();
+    error Raffle_TransferFailed();
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    uint256 private immutable i_entranceFee;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+    uint256 private s_lastTimeStamp;
+    address payable[] private s_players;
+    address private s_recentWinner;
+
+    // Events
+    event EnteredRaffle(address indexed player);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        // custom errors save gas compared to require() statements.
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
+
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert Raffle__NotEnoughTimePassed();
+        }
+        // get random winner
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gas lane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS, // number of confirmations before response
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle_TransferFailed();
+        }
+    }
+
+    // Getter Functions
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
